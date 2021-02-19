1dc0c35ed648f701b768ba9caee69a474838245d
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-19 19:02:47 +0300

    feat: routing by RAW code

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
-const http = require('http');
+const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
 
     // Set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<b> Hello World</b>');
-    res.write('<b> Hello Again</b>');
-    res.end();
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    // send html file
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
