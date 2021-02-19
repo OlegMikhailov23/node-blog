366cbb1e0795cb5ca5b8ad112c078638b70cde46
Oleg Mikhaylov <dkfolkin@gmail.com>
2021-02-19 16:54:32 +0300

    feat: basics

---
+++ global.js
@@ -0,0 +1,17 @@
+// Global obj
+
+//console.log(global)
+//
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int)
+// }, 3000);
+//
+// const int = setInterval(() => {
+//     console.log('Hello im interval')
+// }, 1000)
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
